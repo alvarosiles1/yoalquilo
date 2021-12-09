e450d15bb108ef55f94678fe0ce861bb625b1302
rickypazd <35882906+rickypazd@users.noreply.github.com>
2021-12-08 21:02:08 -0400

    app

---
+++ server/src/Manejador.java
@@ -0,0 +1,18 @@
+import java.io.Console;
+import org.json.JSONObject;
+import Server.SSSAbstract.SSSessionAbstract;
+import Servisofts.SConsole;
+import component.*;
+
+public class Manejador {
+    public static void onMessage(JSONObject obj, SSSessionAbstract session) {
+        if (session != null) {
+            SConsole.log(session.getIdSession(), "\t|\t", obj.getString("component"), obj.getString("type"));
+        }
+        if (!obj.isNull("component")) {
+            switch (obj.getString("component")) {
+                
+            }
+        }
+    }
+}
