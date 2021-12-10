a59afe36924a0a37ecddae65cb391ad5ed8d1523
Alvaro Siles <61075383+alvarosiles11@users.noreply.github.com>
2021-12-09 23:08:02 -0400

    app
    app

---
+++ app/src/Services/contabilidad/Components/empresa/Pages/Lista.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { SIcon, SLoad, SNavigation, SPage, SPopup, STable2, SText, SView , SImage} from 'servisofts-component';
+import FloatButtom from '../../../../../Components/FloatButtom';
+import Parent from '../index'
+import SSocket from 'servisofts-socket'
+class Lista extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+        };
+        this.key_servicio = SNavigation.getParam("key_servicio");
+    }
+
+    getContent() {
+        var data = Parent.Actions.getAll(this.props);
+        if (!data) return <SLoad />;
+        return <STable2
+            header={[
+                { key: "index", label: "#", width: 50 },
+                {
+                    key: "key-foto", label: "Foto", width: 70, center: true, component: (key) => {
+                        return <SView width={40} height={40} onPress={() => {
+                        }}>
+                            <SImage src={SSocket.api.root + Parent.component + "/" + key} />
+                        </SView>
+                    }
+                },
+                { key: "nit", label: "Nit", width: 150 },
+                { key: "razon_social", label: "Razon social", width: 150 },
+                { key: "key-editar", label: "Editar", width: 50, center: true, component: (item) => { return <SView onPress={() => { SNavigation.navigate(Parent.component + "/registro", { key: item }) }}> <SIcon name={"Edit"} width={35} /></SView> } },
+                { key: "key-eliminar", label: "Eliminar", width: 70, center: true, component: (key) => { return <SView width={35} height={35} onPress={() => { SPopup.confirm({ title: "Eliminar", message: "¿Esta seguro de eliminar?", onPress: () => { Parent.Actions.eliminar(data[key], this.key_servicio, this.props) } }) }}> <SIcon name={'Delete'} /> </SView> } },
+
+            ]}
+            filter={(data) => {
+                if (data.estado != 1) return false;
+                return true;
+            }}
+            data={data}
+        />
+    }
+    render() {
+        return (
+            <SPage title={'Lista de ' + Parent.component} disableScroll center>
+                {this.getContent()}
+                <FloatButtom onPress={() => {
+                    SNavigation.navigate(Parent.component + "/registro", { key_servicio: this.key_servicio })
+                }} />
+            </SPage>
+        );
+    }
+}
+const initStates = (state) => {
+    return { state }
+};
+export default connect(initStates)(Lista);
---
+++ app/src/Services/contabilidad/Components/empresa/Pages/Registro.js
@@ -0,0 +1,69 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { SButtom, SForm, SHr, SPage, SText, SNavigation, SLoad, SView, SIcon } from 'servisofts-component';
+import Parent from '../index'
+import SSocket from 'servisofts-socket';
+class Registro extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+        };
+        this.key_servicio = SNavigation.getParam("key_servicio");
+        this.key = SNavigation.getParam("key");
+    }
+    getContent() {
+        this.data = {};
+        if (this.key) {
+            this.data = Parent.Actions.getByKey(this.key, this.props);
+            if (!this.data) return <SLoad />
+        }
+        return <SForm
+            ref={(form) => { this.form = form; }}
+            col={"xs-11 sm-9 md-7 lg-5 xl-4"}
+            inputProps={{
+                customStyle: "calistenia"
+            }}
+            inputs={{
+                foto_p: { type: "image", isRequired: false, defaultValue: `${SSocket.api.root}${Parent.component}/${this.key}` },
+                nit: { label: "nit", isRequired: true, defaultValue: this.data["nit"] },
+                razon_social: { label: "Razon social", isRequired: true, defaultValue: this.data["razon_social"] },
+            }}
+            onSubmit={(values) => {
+                if (this.key) {
+                    Parent.Actions.editar({
+                        ...this.data,
+                        ...values
+                    }, this.props);
+                } else {
+                    values.key_servicio = this.key_servicio;
+                    Parent.Actions.registro(values, this.props);
+                }
+            }}
+        />
+    }
+    render() {
+        if (Parent.Actions.getEstado("registro", this.props) == "exito" || Parent.Actions.getEstado("editar", this.props) == "exito") {
+            Parent.Actions.resetEstado(this.props);
+            this.form.uploadFiles(`${SSocket.api.root}upload/${Parent.component}/${this.key}`);
+            SNavigation.goBack();
+        }
+        return (
+            <SPage title={'Registro de ' + Parent.component} center>
+                <SView height={30}></SView>
+                {this.getContent()}
+                <SHr />
+                <SButtom
+                    style={{ color: '#fff' }}
+                    props={{
+                        type: "outline"
+                    }}
+                    onPress={() => { this.form.submit() }}
+                >{(this.key ? "Editar" : "Registrar")}</SButtom>
+            </SPage>
+        );
+    }
+}
+const initStates = (state) => {
+    return { state }
+};
+export default connect(initStates)(Registro);
