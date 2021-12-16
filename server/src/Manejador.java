ccc1f656152a5744853447564631da7a2d31e6f0
Alvaro Siles <61075383+alvarosiles11@users.noreply.github.com>
2021-12-16 01:16:31 -0400

    alvaro
    alvaro

--- server/src/Manejador.java
+++ server/src/Manejador.java
@@ -2,15 +2,35 @@ import java.io.Console;
 import org.json.JSONObject;
 import Server.SSSAbstract.SSSessionAbstract;
 import Servisofts.SConsole;
+import component.empresa;
+import component.servicio;
+import component.subordinado;
+import component.sucursal;
 
 public class Manejador {
     public static void onMessage(JSONObject obj, SSSessionAbstract session) {
+
         if (session != null) {
             SConsole.log(session.getIdSession(), "\t|\t", obj.getString("component"), obj.getString("type"));
         }
         if (!obj.isNull("component")) {
             switch (obj.getString("component")) {
-                
+                case "empresa":
+                    new empresa(obj, session);
+                    break;
+
+                case "sucursal":
+                    new sucursal(obj, session);
+                    break;
+
+                case "servicio":
+                    new servicio(obj, session);
+                    break;
+
+                case "subordinado":
+                    new subordinado(obj, session);
+                    break;
+
             }
         }
     }
---
+++ server/src/component/sucursal.java
@@ -0,0 +1,75 @@
+package component;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.UUID;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import Server.SSSAbstract.SSSessionAbstract;
+import Servisofts.SPGConect;
+
+public class sucursal {
+    private final static String component = "sucursal";
+
+    public sucursal(JSONObject obj, SSSessionAbstract sesion) {
+        switch (obj.getString("type")) {
+            case "getAll":
+                getAll(obj, sesion);
+                break;
+            case "registro":
+                registro(obj, sesion);
+                break;
+            case "editar":
+                editar(obj, sesion);
+                break;
+        }
+    }
+
+    public void getAll(JSONObject obj, SSSessionAbstract sesion) {
+        try {
+            String consulta = "select jsonb_object_agg(" + component + ".key, to_json(" + component
+                    + ".*)) as json from " + component + " where " + component + ".estado = 1";
+            JSONObject data = SPGConect.ejecutarConsultaObject(consulta);
+            obj.put("data", data);
+            obj.put("estado", "exito");
+        } catch (Exception e) {
+            obj.put("estado", "error");
+            e.printStackTrace();
+        }
+    }
+
+    public void registro(JSONObject obj, SSSessionAbstract sesion) {
+        try {
+            DateFormat formatter = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSSSS");
+            String fecha_on = formatter.format(new Date());
+
+            JSONObject data = obj.getJSONObject("data");
+            data.put("key", UUID.randomUUID().toString());
+            data.put("fecha_on", fecha_on);
+            data.put("estado", 1);
+
+            SPGConect.insertArray(component, new JSONArray().put(data));
+
+            obj.put("data", data);
+            obj.put("estado", "exito");
+        } catch (Exception e) {
+            obj.put("estado", "error");
+            e.printStackTrace();
+        }
+    }
+
+    public void editar(JSONObject obj, SSSessionAbstract sesion) {
+        try {
+
+            JSONObject data = obj.getJSONObject("data");
+            SPGConect.editObject(component, data);
+            obj.put("data", data);
+            obj.put("estado", "exito");
+        } catch (Exception e) {
+            obj.put("estado", "error");
+            e.printStackTrace();
+        }
+    }
+
+}
