d7538afaf8bea81b90860b4dc0e33399cfe73d2b
Alvaro Siles <alvarosiles1@gmail.com>
2022-04-21 20:14:19 -0400

    okid
    okid

--- app/src/Services/yoalquilo/Components/inmueble/Pages/Lista.js
+++ app/src/Services/yoalquilo/Components/inmueble/Pages/Lista.js
@@ -8,8 +8,7 @@ import FloatButtom from '../../../../../Components/FloatButtom';
 class lista extends Component {
     constructor(props) {
         super(props);
-        this.state = {
-        };
+        this.state = {};
     }
 
     getAll() {
@@ -19,28 +18,20 @@ class lista extends Component {
             SNavigation.navigate("inmueble/registro");
             return <SText>No hay inmuebles</SText>
         }
-
         return Object.keys(lista).map(key => {
             var obj = lista[key];
             return <>
                 <SView col={'xs-6 md-4 lg-3 xl-2'} row center border={'transparent'}  >
-
                     <SView width={130} height={150} row center
                         onPress={() => { SNavigation.navigate('inmueble/perfil', { key_inmueble: key }) }}>
-                        <SView style={{
+                        <SView style={{// backgroundColor: "#F6F6F6",
                             width: 120, height: 120, borderRadius: 20,
-                            // backgroundColor: "#F6F6F6",
                         }} center >
-
-                            <SView width={'100%'} height={'100%'} style={{
-                                position: "absolute",
-                            }} center>
+                            <SView width={'100%'} height={'100%'} style={{ position: "absolute", }} center>
                                 <SIcon name={inmueble.Actions.getIcon(obj.tipo_inmueble)} width={80} height={80} fill={STheme.color.primary} />
                             </SView>
-
                             <SIcon name={'IconMenuBg'} width={"100%"} />
                         </SView>
-
                         <SView col={'xs-12'} height={18} center backgroundColor={'transparent'}   >
                             <SText fontSize={16} font={"Roboto-Light"} color={'#212121'} style={{ textTransform: "capitalize" }}>{obj.descripcion}</SText>
                             <SText fontSize={12} font={"Roboto-Light"} color={'#BEB2B2'} style={{ textTransform: "capitalize" }}>Ubicación: {obj.direccion}</SText>
@@ -48,8 +39,6 @@ class lista extends Component {
                     </SView>
                     <SView col={'xs-12'} height={40} />
                 </SView>
-
-
             </>
         })
     }
